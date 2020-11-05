1c2a45c2b22b99786376a02609187c972ad99de1
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-05 10:07:05 -0500

    useEffect forward/reset song index when ui triggers, <Player currentSongIndex, setCurrentSongIndex, nextSongIndex songs

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,5 @@
 import "./app.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // FIXTURES
 import SongsList from "../fixtures/";
@@ -9,12 +9,29 @@ import Player from "../components/Player";
 
 export default function App() {
   const [songs, setSongs] = useState(SongsList);
+
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
+  // forward/reset song index when ui triggers
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+
   return (
     <div className="app">
-      <Player song={songs[currentSongIndex]} nextSong={songs[nextSongIndex]} />
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
     </div>
   );
 }
