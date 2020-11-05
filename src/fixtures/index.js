57ebc084f8de5d9dbdf9da504f15e8096f35d9ec
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-05 09:57:07 -0500

    8 songs Rico Metal inspo beats

--- src/fixtures/index.js
+++ src/fixtures/index.js
@@ -1,26 +1,56 @@
 export default [
   {
-    title: "Forget me too ft. Halsey",
-    artist: "Machine Gun Kelly",
+    title: "Valhalla",
+    artist: "Rico Metal",
     img_src: "./public/assets/images/song-1.jpg",
-    src: "./music/on-n-on.mp3",
+    src: "./music/(Free) J.I.D x Bas x Boogie Type Beat PRECISION.mp3",
   },
   {
     title: "Song 2",
-    artist: "Artist 2",
+    artist: "Rico Metal",
     img_src: "./images/song-2.jpg",
-    src: "./music/somebody-new.mp3",
+    src:
+      "./music/[FREE FOR PROFIT] Rosalía X Bad Bunny X Drake Type Beat Siempre.mp3",
   },
   {
     title: "Song 3",
-    artist: "Artist 3",
+    artist: "Rico Metal",
     img_src: "./images/song-3.jpg",
-    src: "./music/on-n-on.mp3",
+    src: "./music/[FREE] DaBaby Type Beat - Flash.mp3",
   },
   {
     title: "Song 4",
-    artist: "Artist 4",
+    artist: "Rico Metal",
     img_src: "./images/song-4.jpg",
-    src: "./music/on-n-on.mp3",
+    src:
+      "./music/[FREE] DABABY x BBNO$ x YUNG GRAVY TYPE BEAT 2019 SALSA (PROD. TREETIME).mp3",
+  },
+  {
+    title: "Song 5",
+    artist: "Rico Metal",
+    img_src: "./images/song-5.jpg",
+    src:
+      "./music/[FREE] DABABY x BBNO$ x YUNG GRAVY TYPE BEAT 2019 SALSA (PROD. TREETIME).mp3",
+  },
+  {
+    title: "Song 6",
+    artist: "Rico Metal",
+    img_src: "./images/song-6.jpg",
+    src:
+      "./music/[FREE] DABABY x BBNO$ x YUNG GRAVY TYPE BEAT 2019 SALSA (PROD. TREETIME).mp3",
+  },
+  {
+    title: "Song 7",
+    artist: "Rico Metal",
+    img_src: "./images/song-7.jpg",
+    src:
+      "./music/[FREE] DABABY x BBNO$ x YUNG GRAVY TYPE BEAT 2019 SALSA (PROD. TREETIME).mp3",
+  },
+  {
+    title: "Song 8",
+    artist: "Rico Metal",
+    img_src: "./images/song-8.jpg",
+    src:
+      "./music/[FREE] DABABY x BBNO$ x YUNG GRAVY TYPE BEAT 2019 SALSA (PROD. TREETIME).mp3",
   },
 ];
