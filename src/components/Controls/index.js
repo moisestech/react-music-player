60bcfd581108f27501fae5e58d31404952376554
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-06 14:10:19 -0500

    refactoring onClick playButton for debugging

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -10,17 +10,26 @@ import {
   faBackward,
 } from "@fortawesome/free-solid-svg-icons";
 
-export default function Controls({ isPlaying }) {
+export default function Controls({ isPlaying, setIsPlaying, SkipSong }) {
+  console.log("Controls!!!");
+
+  // console.log("isPlaying :>> ", isPlaying);
+
+  const HandlePlayButton = () => {
+    console.log("isPlaying :>> HANDLE PLAYBUTTON", isPlaying);
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div className="c-player-controls">
-      <button className="skip-btn">
+      <button className="skip-btn" onClick={() => SkipSong(false)}>
         <FontAwesomeIcon icon={faBackward} />
       </button>
-      <button className="play-btn">
+      <button className="play-btn" onClick={HandlePlayButton}>
         <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
       </button>
       <button className="skip-btn">
-        <FontAwesomeIcon icon={faForward} />
+        <FontAwesomeIcon icon={faForward} onClick={() => SkipSong(true)} />
       </button>
     </div>
   );
@@ -28,4 +37,6 @@ export default function Controls({ isPlaying }) {
 
 Controls.propTypes = {
   isPlaying: PropTypes.bool.isRequired,
+  setIsPlaying: PropTypes.func.isRequired,
+  SkipSong: PropTypes.func.isRequired,
 };
