022e9b8c9b9acd48b6316b3ed52d79d5308e8b7b
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-06 18:08:00 -0500

    set default args for currentSongIndex, nextSongIndex, useEffect isPlaying, SkipSong const func, display audio <Details><Controls/>

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,28 +1,79 @@
+import { useState, useRef, useEffect } from "react";
 import "./player.css";
-import PropTypes from "prop-types";
+import PropTypes, { number } from "prop-types";
 
 // COMPONENTS
 import Controls from "../Controls";
 import Details from "../Details";
 
-export default function Player({ song, nextSong }) {
+export default function Player({
+  currentSongIndex = 0,
+  setCurrentSongIndex,
+  nextSongIndex = 1,
+  songs,
+}) {
+  const audioEl = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioEl.current.play();
+    } else {
+      audioEl.current.pause();
+    }
+  }, [isPlaying]);
+
+  // next/back index from songs[]
+  const SkipSong = (forwards = true) => {
+    if (forwards) {
+      setCurrentSongIndex(() => {
+        let temp = currentSongIndex;
+        temp++;
+
+        if (temp > songs.length - 1) {
+          temp = 0;
+        }
+
+        return temp;
+      });
+    } else {
+      setCurrentSongIndex(() => {
+        let temp = currentSongIndex;
+        temp--;
+
+        if (temp < 0) {
+          temp = songs.length - 1;
+        }
+
+        return temp;
+      });
+    }
+  };
+
   return (
     <div className="c-player">
-      <audio></audio>
+      <audio src={songs[currentSongIndex].src} ref={audioEl}></audio>
       <h4>Playing now</h4>
-      <Details song={song} />
-      <Controls isPlaying={false} />
+      <Details song={songs[currentSongIndex]} />
+      <Controls
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        SkipSong={SkipSong}
+      />
       <p>
-        Next up:{" "}
+        <strong>Next up:</strong>
         <span>
-          {nextSong.title} by {nextSong.artist}
+          {songs[nextSongIndex].title} by {songs[nextSongIndex].artist}
         </span>
       </p>
     </div>
   );
 }
 
+// Test default values
 Player.propTypes = {
-  song: PropTypes.object.isRequired,
-  nextSong: PropTypes.object.isRequired,
+  currentSongIndex: PropTypes.number,
+  setCurrentSongIndex: PropTypes.func.isRequired,
+  nextSongIndex: PropTypes.number,
+  songs: PropTypes.array.isRequired,
 };
