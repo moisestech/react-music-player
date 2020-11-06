4a35b6156a89d5fa36083359efde8a0c6136363d
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-06 18:08:19 -0500

    update fixtures to match new songs

--- src/fixtures/index.js
+++ src/fixtures/index.js
@@ -2,55 +2,60 @@ export default [
   {
     title: "Valhalla",
     artist: "Rico Metal",
-    img_src: "./public/assets/images/song-1.jpg",
-    src: "./music/(Free) J.I.D x Bas x Boogie Type Beat PRECISION.mp3",
+    img_src:
+      "https://raw.githubusercontent.com/moisestech/react-music-player/main/public/assets/images/rico_metal_image_3.JPG",
+    src: "../../public/assets/music/RicoMetal_PandemicSessions.mp3",
   },
   {
-    title: "Song 2",
+    title: "Down by the River",
     artist: "Rico Metal",
-    img_src: "./images/song-2.jpg",
-    src:
-      "./music/[FREE FOR PROFIT] Rosalía X Bad Bunny X Drake Type Beat Siempre.mp3",
+    img_src:
+      "https://raw.githubusercontent.com/moisestech/react-music-player/main/public/assets/images/RicoMetal.png",
+    src: "../../public/assets/music/RicoMetal_Valhalla_1.mp3",
   },
   {
-    title: "Song 3",
+    title: "Down by the River",
     artist: "Rico Metal",
-    img_src: "./images/song-3.jpg",
-    src: "./music/[FREE] DaBaby Type Beat - Flash.mp3",
+    img_src:
+      "https://raw.githubusercontent.com/moisestech/react-music-player/main/public/assets/images/rico_metal_image_3.JPG",
+    src: "../../public/assets/music/[FREE] DaBaby Type Beat - Flash.mp3",
   },
   {
     title: "Song 4",
     artist: "Rico Metal",
-    img_src: "./images/song-4.jpg",
+    img_src:
+      "https://raw.githubusercontent.com/moisestech/react-music-player/main/public/assets/images/rico_metal_pandemic_jam_sessions.png",
     src:
-      "./music/[FREE] DABABY x BBNO$ x YUNG GRAVY TYPE BEAT 2019 SALSA (PROD. TREETIME).mp3",
+      "../../public/assets/music/[FREE] DABABY x BBNO$ x YUNG GRAVY TYPE BEAT 2019 SALSA (PROD. TREETIME).mp3",
   },
   {
     title: "Song 5",
     artist: "Rico Metal",
-    img_src: "./images/song-5.jpg",
+    img_src:
+      "https://raw.githubusercontent.com/moisestech/react-music-player/main/public/assets/images/song-1.jpg",
     src:
-      "./music/[FREE] DABABY x BBNO$ x YUNG GRAVY TYPE BEAT 2019 SALSA (PROD. TREETIME).mp3",
+      "../../public/assets/music/[FREE] TRAVIS SCOTT x DON TOLIVER x JACKBOYS TYPE BEAT 2020 ABSENT (PROD. AFFYAVENUE x TREETIM.mp3",
   },
   {
     title: "Song 6",
     artist: "Rico Metal",
     img_src: "./images/song-6.jpg",
     src:
-      "./music/[FREE] DABABY x BBNO$ x YUNG GRAVY TYPE BEAT 2019 SALSA (PROD. TREETIME).mp3",
+      "../../public/assets/music/Bad Bunny x Guitar type beat Guitar VibeProd by BoneYouaFool88.mp3",
   },
   {
     title: "Song 7",
     artist: "Rico Metal",
-    img_src: "./images/song-7.jpg",
+    img_src:
+      "https://raw.githubusercontent.com/moisestech/react-music-player/main/public/assets/images/song-1.jpg",
     src:
-      "./music/[FREE] DABABY x BBNO$ x YUNG GRAVY TYPE BEAT 2019 SALSA (PROD. TREETIME).mp3",
+      "../../public/assets/music/Don Toliver x Travis Scott Type Beat Winchester.mp3",
   },
   {
     title: "Song 8",
     artist: "Rico Metal",
-    img_src: "./images/song-8.jpg",
-    src:
-      "./music/[FREE] DABABY x BBNO$ x YUNG GRAVY TYPE BEAT 2019 SALSA (PROD. TREETIME).mp3",
+    img_src:
+      "https://raw.githubusercontent.com/moisestech/react-music-player/main/public/assets/images/song-1.jpg",
+    src: "../../public/assets/music/Nombre - Flamenco x Rosalia Type Beat.mp3",
   },
 ];
